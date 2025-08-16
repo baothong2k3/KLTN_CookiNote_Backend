508b090a16da43ca5acaa7ae64d9d6be7e3bfa4f
MSI\PC BAO THONG <baothong15082003@gmail.com>
2025-08-16 19:51:36 +0700

    frist commit

---
+++ src/main/java/fit/kltn_cookinote_backend/KltnCookiNoteBackendApplication.java
@@ -0,0 +1,13 @@
+package fit.kltn_cookinote_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KltnCookiNoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KltnCookiNoteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fit/kltn_cookinote_backend/KltnCookiNoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.kltn_cookinote_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltnCookiNoteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
