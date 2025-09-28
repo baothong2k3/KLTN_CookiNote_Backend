b2714fda2a8865bfdf90dca29fe64be051c6e6a5
MSI\PC BAO THONG <baothong15082003@gmail.com>
2025-09-28 23:18:04 +0700

    [validation] Cập nhật validation

--- src/main/java/fit/kltn_cookinote_backend/dtos/request/ShoppingListUpsertRequest.java
+++ src/main/java/fit/kltn_cookinote_backend/dtos/request/ShoppingListUpsertRequest.java
@@ -1,9 +1,14 @@
 package fit.kltn_cookinote_backend.dtos.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ShoppingListUpsertRequest(
-        @NotBlank String ingredient,
+        @NotBlank(message = "Tên nguyên liệu không được trống.")
+        @Size(max = 100, message = "Tên nguyên liệu tối đa 100 ký tự.")
+        String ingredient,
+
+        @Size(max = 50, message = "Số lượng tối đa 50 ký tự.")
         String quantity
 ) {
 }
