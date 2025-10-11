c2364d2ddf310dbdd8393bc75c5d361366b3624c
Đặng Bảo Thông <baothong.work@gmail.com>
2025-10-11 23:22:22 +0700

    [RecipeCoverImageHistory] Cập nhật logic upload và updateCover

---
+++ src/main/java/fit/kltn_cookinote_backend/entities/RecipeCoverImageHistory.java
@@ -0,0 +1,33 @@
+/*
+ * @ (#) RecipeCoverImageHistory.java    1.0    11/10/2025
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+package fit.kltn_cookinote_backend.entities;/*
+ * @description:
+ * @author: Bao Thong
+ * @date: 11/10/2025
+ * @version: 1.0
+ */
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "recipe_cover_image_history")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RecipeCoverImageHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    @Column(name = "image_url", length = 2048, nullable = false)
+    private String imageUrl;
+}
